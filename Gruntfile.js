bee66916d49c64ee6f67a53148fa483357b6c184
Nik Coughlin <nik@mojule.co.nz>
2015-06-01 13:51:35 +1200

    tidy
    unclutter root

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,18 @@ module.exports = function( grunt ){
   grunt.initConfig({
     pkg: grunt.file.readJSON( 'package.json' ),
     browserify: {
-      '/src/jquery.h5on.js': [ 'jqyery.h5on.js' ]
-    }
+      '/src/jquery.h5on.js': [ './jqyery.h5on.js' ]
+    },
+    uglify: {
+      dist: {
+        files: {
+          'h5on.min.js': [ 'h5on.js' ],
+          'jquery.h5on.min.js': [ 'jquery.h5on.js' ]
+        }
+      }
+    },    
   });
   grunt.loadNpmTasks( 'grunt-browserify' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.registerTask( 'default', [ 'browserify', 'uglify' ] );
 }
